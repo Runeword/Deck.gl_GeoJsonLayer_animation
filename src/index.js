b1ada215e6fcee6ecce3cd510c876b6b63e9b4a4
Runeword <cha.cascio@gmail.com>
2022-05-04 12:45:10 +0200

    feat(index): add transition to the geojson layer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Deck } from "@deck.gl/core";
 import { MapboxLayer } from "@deck.gl/mapbox";
-import { ScatterplotLayer } from "@deck.gl/layers";
+import { GeoJsonLayer } from "@deck.gl/layers";
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken =
@@ -9,47 +9,186 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v9",
-  center: [-74.5, 40],
+  center: [ 2.368057803, 48.866637048 ],
   controller: true,
-  zoom: 15
+  zoom: 18,
+  pitch: 40
 });
 
 const deck = new Deck({
   gl: map.painter.context.gl,
   layers: [
-    new ScatterplotLayer({
-      id: "my-scatterplot",
-      data: [{ position: [-74.5, 40], size: 100 }],
-      getPosition: (d) => d.position,
-      getRadius: (d) => d.size,
-      getFillColor: [255, 0, 0],
-      pickable: true,
-      onHover: (info) => {
-        console.log("info", info);
+    new GeoJsonLayer({
+    id: 'buildingHeight',
+    data: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Polygon',
+            coordinates: [
+              [
+                [
+                  2.368057803,
+                  48.866637048
+                ],
+                [
+                  2.36810017,
+                  48.86673183
+                ],
+                [
+                  2.368179798,
+                  48.866716156
+                ],
+                [
+                  2.36819564,
+                  48.86675312
+                ],
+                [
+                  2.368215589,
+                  48.866742643
+                ],
+                [
+                  2.368295888,
+                  48.86670023
+                ],
+                [
+                  2.368294547,
+                  48.866698218
+                ],
+                [
+                  2.368250056,
+                  48.866600608
+                ],
+                [
+                  2.368181648,
+                  48.866613557
+                ],
+                [
+                  2.368057803,
+                  48.866637048
+                ]
+              ]
+            ]
+          },
+          properties: {
+            h: 17.8,
+          }
+        }
+      ]
+    },
+    extruded: true,
+    elevationScale: 0,
+    getElevation: ({properties}) => {
+        console.log('properties', properties)
+        return properties.h
+    },
+    transitions: {
+      elevationScale: {
+        duration: 1000,
+        type: 'interpolation'
       }
-    })
+    },
+    filled: true,
+    wireframe: true,
+    autoHighlight: true,
+    pickable: true,
+    visible: true,
+    minZoom: 17,
+    opacity: 0.8
+  })
   ]
 });
 
 // wait for map to be ready
 map.on("load", () => {
   // add to mapbox
-  map.addLayer(new MapboxLayer({ id: "my-scatterplot", deck }));
+  map.addLayer(new MapboxLayer({ id: "buildingHeight", deck }));
 
   // update the layer
   deck.setProps({
     layers: [
-      new ScatterplotLayer({
-        id: "my-scatterplot",
-        data: [{ position: [-74.5, 40], size: 100 }],
-        getPosition: (d) => d.position,
-        getRadius: (d) => d.size,
-        getFillColor: [0, 0, 255],
-        pickable: true,
-        onHover: (info) => {
-          console.log("info", info);
+      new GeoJsonLayer({
+    id: 'buildingHeight',
+    data: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Polygon',
+            coordinates: [
+              [
+                [
+                  2.368057803,
+                  48.866637048
+                ],
+                [
+                  2.36810017,
+                  48.86673183
+                ],
+                [
+                  2.368179798,
+                  48.866716156
+                ],
+                [
+                  2.36819564,
+                  48.86675312
+                ],
+                [
+                  2.368215589,
+                  48.866742643
+                ],
+                [
+                  2.368295888,
+                  48.86670023
+                ],
+                [
+                  2.368294547,
+                  48.866698218
+                ],
+                [
+                  2.368250056,
+                  48.866600608
+                ],
+                [
+                  2.368181648,
+                  48.866613557
+                ],
+                [
+                  2.368057803,
+                  48.866637048
+                ]
+              ]
+            ]
+          },
+          properties: {
+            h: 17.8,
+          }
         }
-      })
+      ]
+    },
+    extruded: true,
+    elevationScale: 5,
+    getElevation: ({properties}) => {
+          console.log('properties', properties)
+          return properties.h
+    },
+    transitions: {
+      elevationScale: {
+        duration: 1000,
+        type: 'interpolation'
+      }
+    },
+    filled: true,
+    wireframe: true,
+    autoHighlight: true,
+    pickable: true,
+    visible: true,
+    minZoom: 17,
+    opacity: 0.8
+  })
     ]
   });
 });
